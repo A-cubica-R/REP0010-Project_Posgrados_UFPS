986ba1bb4a1340928a3791830d40bc4874dc8f30
Jose Luis Jiménez <joseluisjb@ufps.edu.co>
2026-04-28 16:43:05 -0500

    Cohorte implementada

---
+++ api-core/src/main/java/ufps/edu/co/domain/exceptions/errorcodes/CohorteErrorCode.java
@@ -0,0 +1,25 @@
+package ufps.edu.co.domain.exceptions.errorcodes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import ufps.edu.co.domain.exceptions.BaseErrorCode;
+
+@Getter
+@AllArgsConstructor
+public enum CohorteErrorCode implements BaseErrorCode {
+    COHORTE_NOT_FOUND("COHORTE_NOT_FOUND", "Cohorte no encontrada"),
+    COHORTE_DUPLICADO("COHORTE_DUPLICADO", "Cohorte duplicada");
+
+    private final String code;
+    private final String defaultMessage;
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getDefaultMessage() {
+        return defaultMessage;
+    }
+}
