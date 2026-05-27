ff3566c72739a755042798c5bd511d33cdab491b
Gabriel Pinto <camilo1234299@gmail.com>
2026-05-27 01:23:43 -0500

    listar todos los programas con id

--- backend/api-rest/src/main/java/ufps/edu/co/controllers/cases/aspirante/InscripcionCase.java
+++ backend/api-rest/src/main/java/ufps/edu/co/controllers/cases/aspirante/InscripcionCase.java
@@ -1,12 +1,17 @@
 package ufps.edu.co.controllers.cases.aspirante;
 
 import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ufps.edu.co.processor.crud.ProgramaProcessor;
+import ufps.edu.co.records.output.entity.ProgramaListadoOutput;
+import ufps.edu.co.records.output.entity.ProgramaOutput;
 import ufps.edu.co.records.output.entity.RequisitoDocumentoOutput;
 // import ufps.edu.co.rest.services.TipodocumentoService;
 
@@ -14,8 +19,19 @@ import ufps.edu.co.records.output.entity.RequisitoDocumentoOutput;
 @RequestMapping(value = "/inscripciones", produces = MediaType.APPLICATION_JSON_VALUE)
 public class InscripcionCase {
 
+    @Autowired
+    private ProgramaProcessor programaProcessor;
+
     // @Autowired
     // private TipodocumentoService tipodocumentoService;
+
+    @GetMapping("/programas")
+    public ResponseEntity<List<ProgramaListadoOutput>> getProgramas() {
+        List<ProgramaListadoOutput> programas = programaProcessor.findAll().stream()
+                .map(this::toProgramaListadoOutput)
+                .toList();
+        return ResponseEntity.ok(programas);
+    }
 
     @GetMapping("/requisitos")
     public ResponseEntity<List<RequisitoDocumentoOutput>> getRequisitos() {
@@ -30,4 +46,13 @@ public class InscripcionCase {
         // return ResponseEntity.ok(requisitos);
         throw new UnsupportedOperationException("Operación no soportada: reimplementación pendiente.");
     }
+
+    private ProgramaListadoOutput toProgramaListadoOutput(ProgramaOutput programa) {
+        return ProgramaListadoOutput.builder()
+                .id(programa.id())
+                .nombre(programa.nombre())
+                .build();
+    }
+
+    
 }
