733b17d4f0721cc9a5dff0e125d94226cbca7095
adolfoalejandroar <adolfoalejandroar@ufps.edu.co>
2026-05-30 16:44:14 -0500

    Corregidas dependencias sin usar

--- backend/api-core/src/main/java/ufps/edu/co/processor/crud/ValoresglobalesProcessor.java
+++ backend/api-core/src/main/java/ufps/edu/co/processor/crud/ValoresglobalesProcessor.java
@@ -5,7 +5,6 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
